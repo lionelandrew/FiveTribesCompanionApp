2ff2a97828b1531e0a621fc1fbdfe709fbd20b3d
Lionel Sy <lionely.sy@gmail.com>
2014-12-09 10:38:59 -0500

    Added textures

--- FiveTribes/Assets/FiveTribesCalculator.cs
+++ FiveTribes/Assets/FiveTribesCalculator.cs
@@ -1,15 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FiveTribesCalculator : MonoBehaviour {
 
-	// Use this for initialization
+    public GameObject[] playerOneInputs;
+    public GameObject[] playerTwoInputs;
+    public GameObject[] playerThreeInputs;
+    public GameObject[] playerFourInputs;
+
+    public InputField playerOneName;
+    public InputField playerTwoName;
+    public InputField playerThreeName;
+    public InputField playerFourName;
+
+    public Text playerOneTotalScore;
+    public Text playerTwoTotalScore;
+    public Text playerThreeTotalScore;
+    public Text playerFourTotalScore;
+
+    public GameObject ClearScore;
+
 	void Start () {
-	
+        playerOneInputs = GameObject.FindGameObjectsWithTag("PlayerOne");
+        playerTwoInputs = GameObject.FindGameObjectsWithTag("PlayerTwo");
+        playerThreeInputs = GameObject.FindGameObjectsWithTag("PlayerThree");
+        playerFourInputs = GameObject.FindGameObjectsWithTag("PlayerFour");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void CalculateScore()
+    {
+        CalculatePlayerScore(playerOneName, playerOneInputs, playerOneTotalScore);
+        CalculatePlayerScore(playerTwoName, playerTwoInputs, playerTwoTotalScore);
+        CalculatePlayerScore(playerThreeName, playerThreeInputs, playerThreeTotalScore);
+        CalculatePlayerScore(playerFourName, playerFourInputs, playerFourTotalScore);
+    }
+
+    public void ClearScoreBoard()
+    {
+        ClearPlayerScore(playerOneName, playerOneInputs, playerOneTotalScore);
+        ClearPlayerScore(playerTwoName, playerTwoInputs, playerTwoTotalScore);
+        ClearPlayerScore(playerThreeName, playerThreeInputs, playerThreeTotalScore);
+        ClearPlayerScore(playerFourName, playerFourInputs, playerFourTotalScore);
+    }
+
+    void CalculatePlayerScore(InputField playerName, GameObject[] playerInput, Text playerTotalScore)
+    {
+        if (!string.IsNullOrEmpty(playerName.value))
+        {
+            Debug.Log("here");
+            int playerScore = 0;
+
+            for (int i = 0; i < playerInput.Length; i++)
+            {
+                if (string.IsNullOrEmpty(playerInput[i].GetComponentInChildren<InputField>().value))
+                    playerScore += 0;
+                else
+                    playerScore += int.Parse(playerInput[i].GetComponentInChildren<InputField>().value);
+            }
+
+            playerTotalScore.text = playerScore.ToString();
+        }
+
+        ClearScore.SetActive(true);
+    }
+
+    void ClearPlayerScore(InputField playerName, GameObject[] playerInput, Text playerTotalScore)
+    {
+        playerName.value = "";
+
+        for (int i = 0; i < playerInput.Length; i++)
+        {
+            playerInput[i].GetComponentInChildren<InputField>().value = "";
+        }
+
+        playerTotalScore.text = "0";
+
+        ClearScore.SetActive(false);
+    }
+
 }
